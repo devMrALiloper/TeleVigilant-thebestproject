8c28098d781d687417b38a4a5ef2eba282721884
DRAGON <www.mohammad.blogfa111@gmail.com>
2016-07-12 23:52:45 +0430

    Add files via upload

--- plugins/lock_username.lua
+++ plugins/lock_username.lua
@@ -1,26 +1,22 @@
-local function run(msg, matches)
-    if is_owner(msg) then
-        return
-    end
+local function run(msg)
+
     local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-        if data[tostring(msg.to.id)]['settings'] then
-            if data[tostring(msg.to.id)]['settings']['lock_username'] then
-                lock_username = data[tostring(msg.to.id)]['settings']['lock_username']
-            end
-        end
-    end
-    local chat = get_receiver(msg)
-    local user = "user#id"..msg.from.id
-    if lock_username == "yes" then
-        delete_msg(msg.id, ok_cb, true)
-    end
+
+     if data[tostring(msg.to.id)]['settings']['lock_username'] == '✅' then
+
+
+if msg.to.type == 'channel' and not is_momod(msg) then
+	delete_msg(msg.id,ok_cb,false)
+
+        return 
+      end
+   end
 end
- 
-return {
-  patterns = {
-    "@(.*)",
-    "@"
-  },
-  run = run
-}
+
+return {patterns = {
+      "@",
+      "^@[%a%d]",
+}, run = run}
+
+--By @alireza_PT
+--channel : @create_antispam_bot
