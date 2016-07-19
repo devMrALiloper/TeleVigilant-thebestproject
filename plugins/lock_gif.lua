a8080beb2567c4921cff96149309afa5a72f0e4a
aQa.Ali <khorramiali80@gmail.com>
2016-07-19 20:52:26 +0430

    Update lock_gif.lua

--- plugins/lock_gif.lua
+++ plugins/lock_gif.lua
@@ -17,7 +17,7 @@ local function run(msg, matches)
     end
     local chat = get_receiver(msg)
     local user = "user#id"..msg.from.id
-    if lock_gif == "🔒" then
+    if lock_gif == "✅" then
         send_large_msg(get_receiver(msg), "User @" .. msg.from.username .. " Gif & files share is not allowed here!")
         chat_del_user(chat, user, ok_cb, true)
     end
@@ -29,3 +29,4 @@ return {
  },
   run = run
 }
+--Edited by @Mr_AL_i
