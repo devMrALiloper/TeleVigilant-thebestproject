06749317cc68960b50c5258df7c5cd0526e13438
aQa.Ali <khorramiali80@gmail.com>
2016-07-19 22:13:51 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -1854,7 +1854,7 @@ local function run(msg, matches)
 				resolve_username(username,  callbackres, cbres_extra)
 			else
 				savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested SuperGroup ID")
-				return "SuperGroup ID for " ..string.gsub(msg.to.print_name, "_", " ").. ":\n\n"..msg.to.id
+				return "⭕️SuperGroup Name: " ..string.gsub(msg.to.print_name, "_", " ").. "\n\n🆔SuperGroup ID: "..msg.to.id
 			end
 		end
 
