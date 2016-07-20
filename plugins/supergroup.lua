87fdec18415fdd1343609de0b8e04bfdd5b4f387
aQa.Ali <khorramiali80@gmail.com>
2016-07-20 23:29:15 +0430

    Update supergroup.lua

--- plugins/supergroup.lua
+++ plugins/supergroup.lua
@@ -2708,7 +2708,7 @@ local function run(msg, matches)
 		end
 
 		if matches[1] == 'help' and not is_owner(msg) then
-			text = "Mods Only"
+			return super_help()
 			reply_msg(msg.id, text, ok_cb, false)
 		elseif matches[1] == 'help' and is_owner(msg) then
 			local name_log = user_print_name(msg.from)
