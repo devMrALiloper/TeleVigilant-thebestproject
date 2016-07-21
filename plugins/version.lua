156aac14b437212ddc99bf36e712827fa5de5946
aQa.Ali <khorramiali80@gmail.com>
2016-07-21 16:17:50 +0430

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,7 +2,15 @@ do
 
 function run(msg, matches)
   return [[
-سلام
+  <b>SPIRAN🌐BOT >>VERSION 2.0<< </b>
+An advanced and powerful administration bot based on Source Who #Mr_AL_i Writed on LUA Under The GNU General Public License. 
+EDITED ON TELESEED
+CHANNEL : @SPIRAN_CHANNEL
+Robot ID : @Spiran_TG
+Developer & SUDO : άQά </🅰ιɪ>
+Managers : 
+ATTACKER >@XxattackerxX<
+M-Developer >@Developer_001<
 ]]
 end
 
