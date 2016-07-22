867275ac1b59ed9b27266c3e0853a127b3a62475
aQa.Ali <khorramiali80@gmail.com>
2016-07-22 15:00:29 +0430

    Update Set_Sudo[@Hextor_Ch].lua

--- plugins/Set_Sudo[@Hextor_Ch].lua
+++ plugins/Set_Sudo[@Hextor_Ch].lua
@@ -6,16 +6,16 @@ end
     if tonumber (msg.from.id) == OUR ID then--expample 123456789
        if matches[1]:lower() == "setsudo" then
           table.insert(_config.sudo_users, tonumber(matches[2]))
-      print(matches[2]..' added to sudo users')
+      print(matches[2]..' با موفقیت به لیست افراد سودو اضافه شد.')
      save_config()
      reload_plugins(true)
-      return matches[2]..' added to sudo users'
+      return matches[2]..' با موفقیت به لیست افراد سودو اضافه شد.'
    elseif matches[1]:lower() == "remsudo" then
       table.remove(_config.sudo_users, tonumber(matches[2]))
-      print(matches[2]..' removed from sudo users')
+      print(matches[2]..' با موفقیت از لیست افراد سودو پاک شد.')
      save_config()
      reload_plugins(true)
-      return matches[2]..' removed from sudo users'
+      return matches[2]..' با موفقیت از لیست افراد سودو پاک شد.'
       end
    end
 end
@@ -26,5 +26,5 @@ patterns = {
 },
 run = run
 }
---By REZA
---Our Channel : http://Telegram.me/Hextor_Ch
+--By REZA From Hextor_ch
+--Edited by @Mr_AL_i
