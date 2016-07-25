ecd9470a242cfbdf463c8093a650ecc5061730a9
aQa.Ali <khorramiali80@gmail.com>
2016-07-25 19:28:50 +0430

    Update calc.lua

--- plugins/calc.lua
+++ plugins/calc.lua
@@ -21,8 +21,12 @@ local function run(msg, matches)
 end
 
 return {
-  description = "Calculate math expressions with mathjs API",
-  usage = "!calc [expression]: evaluates the expression and sends the result.",
+description = "commands: / and * and ^ and + and -",
+ usage = {
+  "/calc (Formula) : Calculate a Formula",
+  "calc (Formula) : Calculate a Formula",
+  "/محاسبه (Formula) : Calculate a Formula",
+ },
   patterns = {
     "^(محاسبه) (.*)$",
 	"^(calc) (.*)$",
