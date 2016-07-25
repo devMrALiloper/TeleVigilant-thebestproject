b22059cfb1591b5571311f4757360f10cca674d1
aQa.Ali <khorramiali80@gmail.com>
2016-07-25 11:36:06 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -7,7 +7,7 @@
 
 
    local text = matches[1]
-   local chat = "chat#id"..65802460
+   local chat = "chat#id"..95837751
 
   local sends = send_msg(chat, fuse, ok_cb, true)
   return '✅ Message successfully sent'
