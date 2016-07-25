fb9be9f71c2cf66a7558907c64e47744e70a3423
aQa.Ali <khorramiali80@gmail.com>
2016-07-25 13:46:30 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,42 +1,27 @@
 do
-
-function run(msg, matches)
-
-local fuse = '⭐Dear mohamad\n\n#newfeedback \n\nuser ID▶️ : ' .. msg.from.id .. '\n\nGroup ID▶' .. msg.to.id .. '\nName▶ : ' .. msg.from.print_name ..'\n\nusername▶️ :@' .. msg.from.username  ..'\n\n message text ❓\n\n\n' .. matches[1]
-local fuses = '!printf user#id' .. msg.from.id
-
-
-    local text = matches[1]
- bannedidone = string.find(msg.from.id, '88888888')
-        bannedidtwo =string.find(msg.from.id, '8888888888')
-   bannedidthree =string.find(msg.from.id, '153589494')
-
-
-        print(msg.to.id)
-
-        if bannedidone or bannedidtwo or bannedidthree then                    --for banned people
-                return '❌You are in feedback blacklist!\n talk to @Mr_AL_i'
- else
-
-
-                 local sends0 = send_msg('user#95837751', fuse, ok_cb, false)
-
- return '💡your feedback succesfully recived to @Mr_AL_i'
-
-
-
-end
-
-end
-return {
+ function run(msg, matches)
+ 
+ local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
+ local fuses = '!printf user#id' .. msg.from.id
+ 
+ 
+   local text = matches[1]
+   local chat = "chat#id"..YourChatId 
+   --like : local chat = "chat#id"..95837751
+   
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return 'Sent!'
+ 
+ end
+ end
+ return {
+  
   description = "Feedback",
-
-  usage = "!feedback : send maseage to admins with bot",
+ 
+  usage = "!feedback message",
   patterns = {
-    "^[/#!][Ff]eedback (.*)$"
-
+  "^[!/][Ff]eedback (.*)$"
+ 
   },
   run = run
-}
-
-end
+ }
