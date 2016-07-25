84057edb12d98051448af2db67f01c6006d60208
aQa.Ali <khorramiali80@gmail.com>
2016-07-25 10:43:17 +0430

    Update google.lua

--- plugins/google.lua
+++ plugins/google.lua
@@ -1,12 +1,9 @@
 local function googlethat(query)
-  local api        = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
+  local api = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
   local parameters = "q=".. (URL.escape(query) or "")
-
-  -- Do the request
   local res, code = https.request(api..parameters)
   if code ~=200 then return nil  end
   local data = json:decode(res)
-
   local results = {}
   for key,result in ipairs(data.responseData.results) do
     table.insert(results, {
@@ -18,7 +15,7 @@ local function googlethat(query)
 end
 
 local function stringlinks(results)
-  local stringresults=""
+  local stringresults="نتايج جستجوي گوگل:\n______________________________\n"
   for key,val in ipairs(results) do
     stringresults=stringresults..val[1].." - "..val[2].."\n"
   end
@@ -31,12 +28,12 @@ local function run(msg, matches)
 end
 
 return {
-  description = "Searches Google and send results",
-  usage = "!google [terms]: Searches Google and send results",
+  description = "Searche in Google",
+  usagehtm = '<tr><td align="center">src مورد</td><td align="right">جست و جوی یک لغت یا جمله در گوگل و دریافت لینک 3جستجوی برتر</td></tr>',
+  usage = "src (item) : جستجوي گوگل",
   patterns = {
-    "^گوگل (.*)$",
-    "^!google (.*)$",
-    "^[Gg]oogle (.*)$"
+    "^[Ss]rc (.*)$",
+    "^%.[s|S]rc (.*)$"
   },
   run = run
 }
