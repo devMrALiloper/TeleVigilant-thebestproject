80a23267ee7a92b80c63085b92da3357809d3168
aQa.Ali <khorramiali80@gmail.com>
2016-07-26 17:52:25 +0430

    Add files via upload

---
+++ plugins/help.lua
@@ -0,0 +1,35 @@
+do
+
+function run(msg, matches)
+  return [[
+  SPIRAN??BOT >>VERSION 2.0<<
+  -------------------
+  An advanced and powerful administration bot based on Source Who #Mr_AL_i Writed on LUA Under The GNU General Public License. 
+  -------------------
+  EDITED ON TELESEED
+  -------------------
+  CHANNEL : @SPIRAN_CHANNEL
+  -------------------
+  Robot ID : @Spiran_TG
+  -------------------
+  Developer & SUDO : ?Q? </????>
+  >@Mr_AL_i<
+  -------------------
+  Managers : 
+  ATTACKER >@XxattackerxX<
+  M-Developer >@Developer_001<
+]]
+end
+
+return {
+  description = "",
+  usage = "",
+  patterns = {
+     "^[/!#][Vv]ersion$",
+    "^version$",
+    "^æÑŽä$"
+  },
+  run = run
+}
+
+end
