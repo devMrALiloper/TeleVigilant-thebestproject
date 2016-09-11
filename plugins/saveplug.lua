b4b64d9347c5db076592464fb45dacfc7b67fd55
MrALiCRAKER <khorramiali80@gmail.com>
2016-09-11 17:24:57 +0430

    Update saveplug.lua

--- plugins/saveplug.lua
+++ plugins/saveplug.lua
@@ -1,6 +1,24 @@
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
+local function reload_plugins( )
+  plugins = {}
+  load_plugins()
+end
+
+local function plugin_enabled( name )
+  for k,v in pairs(_config.enabled_plugins) do
+    if name == v then
+      return k
+    end
+  end
+  return false
+end
+
+local function enable_plugin( plugin_name )
+if plugin_enabled(plugin_name) then
+  reload_plugins( )
+else table.insert(_config.enabled_plugins, plugin_name)
+    save_config()
+end
+end
 
 local function saveplug(extra, success, result)
   local msg = extra.msg
@@ -11,29 +29,35 @@ local function saveplug(extra, success, result)
     print('File saving to:', result)
     os.rename(result, file)
     print('File moved to:', file)
+    enable_plugin(name)
+    print('Reloading...')
+    reload_plugins( )
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
 end
-local function run(msg,matches)
+  local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.reply_id then
    local name = matches[2]
       if matches[1] == "save" and matches[2] and is_sudo(msg) then
-load_document(msg.reply_id, saveplug, {msg=msg,name=name})
-        return 'Plugin '..name..' has been saved.'
+        load_document(msg.reply_id, saveplug, {msg=msg,name=name})
+        reply_msg(msg['id'], 'Plugin '..name..' has been saved.', ok_cb, false)
+      end
     end
-end
-end
+  end
+
 return {
+  advan = {
+    "Created by: @janlou & @Alirezame",
+    "Powered by: @SUDO_TM & @AdvanTM",
+    "CopyRight all right reserved",
+  },
   patterns = {
- "^[!/#](save) (.*)$",
+   "^[!/#](save) (.*)$",
   },
   run = run,
 }
 
---Created by: @janlou & @Alirezame
---Powered by: @SUDO_TM & @AdvanTM
---⚠️CopyRight all right reserved⚠️
