b5247ef4896824b830b2d50c8762523dbdabe852
MrALiCRAKER <khorramiali80@gmail.com>
2016-09-11 17:27:39 +0430

    Update delplug.lua

--- plugins/delplug.lua
+++ plugins/delplug.lua
@@ -1,11 +1,17 @@
-function run(msg, matches)
-text = io.popen("cd plugins && rm -rf  " .. matches[1]):read('*all')
-  return text
+
+local function run(msg, matches)
+    if matches[1] == "delplug" and is_sudo(msg) then
+text = io.popen("cd plugins && rm "..matches[2]..".lua")
+reply_msg(msg['id'], "پلاگین ["..matches[2].."] با موفقیت حذف شد", ok_cb, false)
+return text
+end 
 end
-return {
-  patterns = {
-    "^[!/#]remplugin (.*)$"
-  },
-  run = run,
-  moderated = true
+
+return { 
+patterns = {
+ 
+'^[!/](delplug) (.*)$' 
+},
+run = run,
 }
+--@TeleVigilant_Team
