3e79cfd80e508b2c40dcd598b7641aefbb537341
MrALiCRAKER <khorramiali80@gmail.com>
2016-09-11 16:50:42 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {95837751},--Sudo users
+    sudo_users = {95837751,78246181,115222449},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Spiran_TG
